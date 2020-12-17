99e99835fcc819e544773607096c047e7c8d4d85
Nicolas <nicolas.bermell@protonmail.com>
2020-12-17 23:52:48 +0100

    Creado ejercicio algoritmo euclides

---
+++ EjerciciosPython/AlgoritmoDeEuclides.py
@@ -0,0 +1,35 @@
+
+### ALGORITMO DE EUCLIDES ###
+
+print("\n### ALGORITMO DE EUCLIDES ###\n")
+print("Dime dos números: ")
+
+a = int(input(""));
+b = int(input(""));
+
+A = a
+B = b
+
+if(not(a > b)):
+    print("Has introducido dos números inadecuados para el algoritmo de euclides")
+    exit()
+
+resto = -1
+listaCocientes = []
+index = 0
+cociente = 0
+
+while(resto != 0):
+    restoAnterior = resto
+    resto = a%b
+    cociente = a//b
+    listaCocientes.append(cociente)
+    a = b
+    b = resto
+    index = index + 1
+
+print("El máximo común divisor de "+str(A) + " y " + str(B) + " es "+str(restoAnterior))
+
+
+
+print(str(A) + "/" + str(B) + " se puede escribir: q = " + str(listaCocientes))
