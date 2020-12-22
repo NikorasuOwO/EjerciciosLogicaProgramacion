2ad3f40ee357b6d4e402ce6c23dc6e1ff175bc62
NikorasuOwO <nicolas.bermell@protonmail.com>
2020-12-22 15:58:25 +0100

    Añadido programa para ordenar lista

--- EjerciciosPython/AlgoritmoDeEuclides.py
+++ EjerciciosPython/AlgoritmoDeEuclides.py
@@ -12,6 +12,7 @@ B = b
 
 if(not(a > b)):
     print("Has introducido dos números inadecuados para el algoritmo de euclides")
+    input()
     exit()
 
 resto = -1
@@ -30,6 +31,6 @@ while(resto != 0):
 
 print("El máximo común divisor de "+str(A) + " y " + str(B) + " es "+str(restoAnterior))
 
-
-
 print(str(A) + "/" + str(B) + " se puede escribir: q = " + str(listaCocientes))
+
+input()
